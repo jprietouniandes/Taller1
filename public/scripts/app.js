408152215c3c3d115f35ba91749b5885d4aa0f98
Glitch (jprietouniandes-taller1-1) <none>
2020-02-03 00:56:44 +0000

    ⛈🚉 Checkpoint ./public/ic_add_white_24px.svg:10819602/199 ./public/ic_refresh_white_24px.svg:10819602/366 ./public/scripts/app.js:10819602/205 ./public/index.html:10819602/507

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -238,10 +238,4 @@
       *
       *************************************************************************/
 
-      if ('serviceWorker' in navigator) {
-            navigator.serviceWorker
-            .register('./service-worker.js')
-            .then(function() { console.log('Service Worker Registered'); });
-  }
-
 })();
